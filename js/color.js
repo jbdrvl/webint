03e6e2082d3b4a928d2156f19790617bfc86a73f
William CLOT <you@example.com>
2017-12-12 14:01:02 +0100

    Flat design

---
+++ js/color.js
@@ -0,0 +1,17 @@
+var colors = [
+  "#CDB07E", //orange
+  "#7EB7CD", //blue
+  "#B56CB4", //pink
+  "#6C92B5", //blue grayscale
+  "#6CB588", //green
+  "#87B56C", //green2
+  "#D19C93"
+];
+var colourIndex = Math.floor(Math.random() * colors.length);
+//HTML5 local storage
+localStorage.setItem("color",colourIndex);
+// on récupère l'élément
+var elmt = document.getElementById("all");
+
+// on modifie son style
+elmt.style.backgroundColor = colors[colourIndex];
