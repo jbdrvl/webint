044a65b601f1dc2c70503c9ea7dc126d1868c317
William CLOT <you@example.com>
2017-12-12 23:03:48 +0100

    changes again

--- js/color.js
+++ js/color.js
@@ -1,5 +1,5 @@
 var colors = [
-  "#CDB07E", //orange
+  "#4ABDAC", //orange
   "#7EB7CD", //blue
   "#B56CB4", //pink
   "#6C92B5", //blue grayscale
@@ -7,11 +7,16 @@ var colors = [
   "#87B56C", //green2
   "#D19C93"
 ];
+
+var second_color = [
+  "#F7B733"
+]
 var colourIndex = Math.floor(Math.random() * colors.length);
 //HTML5 local storage
 localStorage.setItem("color",colourIndex);
 // on récupère l'élément
-var elmt = document.getElementById("all");
+var html = document.getElementById("all");
+var box = document.getElementById("login-form")
 
 // on modifie son style
-elmt.style.backgroundColor = colors[colourIndex];
+html.style.backgroundColor = colors[0];
