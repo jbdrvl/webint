e5a739caffef5b430cf47db252824a45ef58dc05
JB <jb.durville@telecom-bretagne.eu>
2017-12-15 15:01:40 +0100

    added buttons to color.js

--- js/color.js
+++ js/color.js
@@ -14,16 +14,37 @@ localStorage.setItem("color",colourIndex);
 // on récupère l'élément
 var html = document.getElementById("all");
 var login = document.getElementById("login");
+var buttons = document.getElementsByClassName("my-button");
+console.log(buttons);
 
 // on modifie son style
 html.style.backgroundColor = colors[colourIndex];
 
 //récupère également la couleur pour le bouton login
-login.onmouseover = function()
-{
-  this.style.color = colors[colourIndex];
+try {
+  login.onmouseover = function()
+  {
+    this.style.color = colors[colourIndex];
+  }
+  login.onmouseout = function()
+  {
+    this.style.color = "white";
+  }
+} catch(err) {
+  console.log('[DEBUG] did not find element with ID name: login');
 }
-login.onmouseout = function()
-{
-  this.style.color = "white";
+//same as login for button.my-button
+try {
+  for (var i=0; i<buttons.length; i++) {
+    buttons[i].onmouseover = function()
+    {
+      this.style.color = colors[colourIndex];
+    }
+    buttons[i].onmouseout = function()
+    {
+      this.style.color = "white";
+    }
+  }
+} catch(err) {
+  console.log('[DEBUG] did not find any button');
 }
