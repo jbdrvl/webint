aa2f0f33a84e925fcad36f26827797176b2f79f9
William CLOT <you@example.com>
2017-12-15 13:58:04 +0100

    merging

---
+++ js/color.js
@@ -0,0 +1,29 @@
+var colors = [
+  "#4ABDAC", //orange
+  "#272BAD", //blue
+  "#B56CB4" //pink
+];
+
+var second_color = [
+  "#F7B733",
+  "#D55C53"
+]
+var colourIndex = Math.floor(Math.random() * colors.length);
+//HTML5 local storage
+localStorage.setItem("color",colourIndex);
+// on récupère l'élément
+var html = document.getElementById("all");
+var login = document.getElementById("login");
+
+// on modifie son style
+html.style.backgroundColor = colors[colourIndex];
+
+//récupère également la couleur pour le bouton login
+login.onmouseover = function()
+{
+  this.style.color = colors[colourIndex];
+}
+login.onmouseout = function()
+{
+  this.style.color = "white";
+}
