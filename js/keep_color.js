35d1aae0ad6a0add4c252f5dd0deeba80fe1ae69
William CLOT <you@example.com>
2017-12-15 13:58:29 +0100

    merging

---
+++ js/keep_color.js
@@ -0,0 +1,15 @@
+var colors = [
+  "#4ABDAC", //orange
+  "#272BAD", //blue
+  "#B56CB4", //pink
+  "#6C92B5", //blue grayscale
+  "#6CB588", //green
+  "#87B56C", //green2
+  "#D19C93"
+];
+var colourIndex = localStorage.getItem("color");
+// on récupère l'élément
+var elmt = document.getElementById("all");
+
+// on modifie son style
+elmt.style.backgroundColor = colors[colourIndex];
