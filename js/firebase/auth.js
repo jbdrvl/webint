51f5c5e799c2f80ef39bfa298f0888049070429a
William CLOT <you@example.com>
2018-01-04 17:50:01 +0100

    email verification

--- js/firebase/auth.js
+++ js/firebase/auth.js
@@ -2,6 +2,7 @@ $('document').ready(function () {
   $('#login').click(function () {
     var email = $('#username').val();
     var password = $('#password').val();
+
     firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
       // Handle Errors here.
       var errorCode = error.code;
@@ -16,7 +17,12 @@ $('document').ready(function () {
       if (user) {
         var currentUser = firebase.auth().currentUser;
         //alert(currentUser.email);
-        window.location.replace("home.html");
+        if (currentUser.emailVerified==false) {
+          $('#background').css("visibility","visible");
+          $('#alertinfo').html("Please verify your email address before logging in");
+        } else {
+          window.location.replace("home.html");
+        }
         firebase.auth().signOut();
       } else {
         // No user is signed in.
