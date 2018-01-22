b25f048bec5eb763db50152c85837303d90926d5
William CLOT <you@example.com>
2018-01-22 11:19:28 +0100

    commit

--- js/keep_color.js
+++ js/keep_color.js
@@ -1,7 +1,15 @@
 var colors = [
   "#4ABDAC",
   "#272BAD",
-  "#B56CB4"
+  "#B56CB4",
+  "#5E8285"
+];
+
+var second_color = [
+  "#F7B733",
+  "#D55C53",
+  "#D55C53",
+  "#63B82B"
 ];
 var colourIndex = localStorage.getItem("color");
 // on récupère l'élément
--- js/tabin.js
+++ js/tabin.js
@@ -1,16 +1,20 @@
 var colors = [
   "#4ABDAC",
   "#272BAD",
-  "#B56CB4"
+  "#B56CB4",
+  "#5E8285"
 ];
 
 var second_color = [
   "#F7B733",
   "#D55C53",
-  "#D55C53"
+  "#D55C53",
+  "#63B82B"
 ];
 localStorage.setItem("tab",1);
 var colourIndex = localStorage.getItem("color");
+const mq = window.matchMedia( "(max-device-width: 480px)" );
+
 
 $('#signin').css("border-color",second_color[colourIndex]);
 
@@ -25,14 +29,18 @@ $('document').ready(function () {
     $('#signup').css("font-weight","900");
     $('#form-login-container').css("visibility","hidden");
     $('#form-signup-container').css({visibility:"visible", opacity: 0.0}).animate({opacity: 1.0},200);
-    $('#logo').css("width","30vh");
-    $('#logo').css("height","30vh");
-    $('#logo').css("bottom","35%");
-    $('#login-form').css("top","70%");
-    $('#login-form').css("height","45%");//TODO
-    $('#login-title').css("height","20%");
-    $('#buttons_container').css("height","20%");
     $('#data_login').css("visibility","hidden");
+    if (mq.matches){
+
+    } else {
+      $('#logo').css("width","30vh");
+      $('#logo').css("height","30vh");
+      $('#logo').css("top","15%");
+      $('#login-form').css("top","70%");
+      $('#login-form').css("height","45%");//TODO
+      $('#login-title').css("height","20%");
+      $('#buttons_container').css("height","20%");
+    }
   })
   $('#signup').mouseover(function () {
     $('#signup').css("font-weight","1000");
@@ -43,10 +51,18 @@ $('document').ready(function () {
   $('.input').keypress(function() {
   var textLength = $(this).val().length;
   if (textLength < 13) {
+    if (mq.matches){
+      $(this).css('font-size', '2.5em');
+      $(this).css('margin-right', '1em');
+    } else {
       $(this).css('font-size', '1.5em');
       $(this).css('margin-right', '1em');
+    }
   } else if (textLength > 13) {
+    if (mq.matches){
+    } else {
       $(this).css('font-size', '1.2em');
+    }
   }
  //console.log(textLength);
 });
@@ -61,16 +77,21 @@ $('document').ready(function () {
     $('#signin').css("border-color",second_color[colourIndex]);
     $('#signup').css("font-weight","500");
     $('#signin').css("font-weight","900");
-    $('#logo').css("width","45vh");
-    $('#logo').css("height","45vh");
-    $('#logo').css("bottom","7em");
-    $('#login-form').css("top","75%");
-    $('#login-form').css("height","20%");
     $('#form-login-container').css({visibility:"visible", opacity: 0.0}).animate({opacity: 1.0},200);
     $('#form-signup-container').css("visibility","hidden");
-    $('#login-title').css("height","45%");
-    $('#buttons_container').css("height","45%");
     $('#data_login').css("visibility","visible");
+
+    if (mq.matches) {
+
+    } else {
+      $('#logo').css("width","45vh");
+      $('#logo').css("height","45vh");
+      $('#logo').css("top","15%");
+      $('#login-form').css("top","75%");
+      $('#login-form').css("height","20%");
+      $('#login-title').css("height","45%");
+      $('#buttons_container').css("height","45%");
+    }
   })
   $('#signin').mouseover(function () {
     $('#signin').css("font-weight","1000");
