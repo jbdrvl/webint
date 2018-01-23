8db8364d6cf19bd7dbed87bfbd404dac62e65eb0
William CLOT <you@example.com>
2018-01-23 16:40:32 +0100

    commit

---
+++ js/set_info_colors.js
@@ -0,0 +1,13 @@
+var colors = [
+  "#548B34",
+  "#245F88",
+  "#ba5536",
+  "#777E3F",
+  "#80781F",
+  "#167155",
+  "#4D3565",
+  "#53665D"
+];
+
+$("#first").css("background-color",colors[0]);
+$("#second").css("background-color",colors[1]);
