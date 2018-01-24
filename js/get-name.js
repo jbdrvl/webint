f21e7db724defa3160eec8e5512769700b721164
JB <jb.durville@telecom-bretagne.eu>
2018-01-24 11:04:27 +0100

    fixed a pb with localStorage and username/email

--- js/get-name.js
+++ js/get-name.js
@@ -1,7 +1,20 @@
 var username = localStorage.getItem('username');
 var email = localStorage.getItem('email');
-usernameSpans = document.getElementsByClassName('username-span');
-document.getElementById('email').innerHTML = email;
-for (var i=0; i<usernameSpans.length; i++) {
-  usernameSpans[i].innerHTML = username;
+if (username) {
+  try {
+    usernameSpans = document.getElementsByClassName('username-span');
+    for (var i=0; i<usernameSpans.length; i++) {
+      usernameSpans[i].innerHTML = username;
+    }
+  } catch(err) {
+    console.log("[INFO] no username span on this page.");
+  }
+}
+
+if (email) {
+  try {
+    document.getElementById('email').innerHTML = email;
+  } catch(err) {
+    console.log("[INFO] no email field on this page.");
+  }
 }
